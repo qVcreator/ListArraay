0b85f9920c8cd2d5ae345a89df62d9140b2159f0
Timur <2001rich04@gmail.com>
2022-03-18 15:57:03 +0300

    Add AddLast

--- ListArray/ListArray.cs
+++ ListArray/ListArray.cs
@@ -5,5 +5,31 @@
         public int Length { get;private set; }
 
         private int[] _array;
+
+        public void AddLast(int value)
+        {
+            if(Length >= _array.Length)
+            {
+                IncreaseLengthOfArray();
+            }
+
+            _array[Length] = value;
+            Length++;
+        }
+        private void IncreaseLengthOfArray()
+        {
+            int newLength = (int)(_array.Length * 1.5);
+            int[] newArr = new int[newLength];
+            Copy(newArr);       
+        }
+
+        private void Copy(int[] newArr)
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                newArr[i] = _array[i];
+            }
+            _array = newArr;
+        }
     }
 }
