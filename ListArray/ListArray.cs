42fa16f044d86a79e97a12c5947498f378c5e969
Timur <2001rich04@gmail.com>
2022-03-18 17:04:47 +0300

    Add AddByIndex; DeleteLast; DeletFirst; DeleteByIndex

--- ListArray/ListArray.cs
+++ ListArray/ListArray.cs
@@ -19,7 +19,7 @@
 
         public void AddFirst(int value)
         {
-            if (Length+1 >= _array.Length)
+            if (Length + 1 >= _array.Length)
             {
                 IncreaseLengthOfArray();
             }
@@ -29,20 +29,108 @@
             _array[0] = value;
         }
 
-        private void MoveRightSide()
+        public void AddByIndex(int num, int value)
+        {
+            if (Length + 1 >= _array.Length)
+            {
+                IncreaseLengthOfArray();
+            }
+
+            MoveRightSide(num);
+
+            _array[num] = _array[value];
+        }
+
+        public void DeleteLast()
+        {
+            if (Length < 1)
+            {
+                throw new Exception("List have nothing to delete");
+            }
+            if (Length <= _array.Length / 2)
+            {
+                DecreaseLengthOfArray();
+            }
+            Length--;
+        }
+
+        public void DeletFirst()
+        {
+            if (_array.Length < 1)
+            {
+                throw new Exception("List have nothing to delete");
+            }
+            if (Length <= _array.Length / 2)
+            {
+                DecreaseLengthOfArray();
+            }
+
+            MoveLeftSide();
+        }
+
+        public void DeleteByIndex(int num)
+        {
+            if (Length < 1)
+            {
+                throw new Exception("List have nothing to delete");
+            }
+            if (Length <= _array.Length / 2)
+            {
+                DecreaseLengthOfArray();
+            }
+
+            MoveLeftSideFromAnyPart(num);
+        }
+
+
+        private void MoveRightSide(int index=0)
         {
             int[] newArr = new int[Length+1];
-            for (int i=0; i < Length; i++)
+            for (int i = index; i < Length; i++)
             {
                 newArr[i+1] = _array[i];
             }
             _array = newArr;
         }
+
+        private void MoveLeftSideFromAnyPart(int index = 0)
+        {
+            Length--;
+            int[] newArr = new int[Length];
+            for (int i = 0; i < index; i++)
+            {
+                newArr[i] = _array[i];
+            }
+            for (int i = index; i < Length; i++)
+            {
+                newArr[i] = _array[i+1];
+            }
+            _array = newArr;
+        }
+
+        private void MoveLeftSide()
+        {
+            Length--;
+            int[] newArr = new int[Length];
+            for (int i = 0; i < Length; i++)
+            {
+                newArr[i] = _array[i + 1];
+            }
+            _array = newArr;
+        }
+
         private void IncreaseLengthOfArray()
         {
             int newLength = (int)(_array.Length * 1.5);
             int[] newArr = new int[newLength];
             Copy(newArr);       
+        }
+
+        private void DecreaseLengthOfArray()
+        {
+            int newLength = _array.Length / 3;
+            int[] newArr = new int[newLength];
+            Copy(newArr);
         }
 
         private void Copy(int[] newArr)
