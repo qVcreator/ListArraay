87113b40aca0233bc6bfb086d3107435cd556a1b
Timur <2001rich04@gmail.com>
2022-03-25 13:29:33 +0300

    Add AddLast

--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -2,6 +2,67 @@
 {
     public class LinkedList
     {
+        private Node _root;
 
+        private Node _tail;
+
+        public int this[int index]
+        {
+            get
+            {
+                Node crnt = _root;
+                for (int i = 1; i <= index; i++)
+                {
+                    crnt = crnt.Next;
+                }
+
+                return crnt.Value;
+            }
+
+            set
+            {
+                Node crnt = _root;
+                for (int i = 1; i <= index; i++)
+                {
+                    crnt = crnt.Next;
+                }
+
+                crnt.Value = value;
+            }
+        }
+
+        public int Length
+        {
+            get
+            {
+                int count = 0;
+                Node crnt = _root;
+
+                while (crnt != null)
+                {
+                    crnt = crnt.Next;
+                    count++;
+                }
+
+                return count;
+            }
+            private set
+            {
+            }
+        }
+
+        public void AddLast(int value)
+        {
+            if (_root is null)
+            {
+                _root = new Node(value);
+                _tail = _root;
+            }
+            else
+            {
+                _tail.Next = new Node(value);
+                _tail = _tail.Next;
+            }
+        }
     }
 }
--- LinkedList/Node.cs
+++ LinkedList/Node.cs
@@ -8,5 +8,14 @@ namespace LinkedList
 {
     internal class Node
     {
+        public int Value { get; set; }
+
+        public Node Next { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+            Next = null;
+        }
     }
 }
